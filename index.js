e3a45bc0030b911b178847630a4f159047e67c72
Shinkaruk Anna <sklifa1087@gmail.com>
2026-06-10 12:30:28 +0300

    commit task-3

--- index.js
+++ index.js
@@ -85,3 +85,30 @@ function animal(obj){
     return "This " + obj.color + " " + obj.name + " has " + obj.legs + " legs.";
 
 }
+// Your task is to add a new property usersAnswer to every object in the array questions. The value of usersAnswer should be set to null. The solution should work for array of any length.
+// For example:
+// var questions = [{
+//     question: "What's the currency of the USA?",
+//     choices: ["US dollar", "Ruble", "Horses", "Gold"],
+//     corAnswer: 0
+// }, {
+//     question: "Where was the American Declaration of Independence signed?",
+//     choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+//     corAnswer: 0
+// }];
+// After adding the property the result should be:
+// var questions = [{
+//     question: "What's the currency of the USA?",
+//     choices: ["US dollar", "Ruble", "Horses", "Gold"],
+//     corAnswer: 0,
+//     usersAnswer: null
+// }, {
+//     question: "Where was the American Declaration of Independence signed?",
+//     choices: ["Philadelphia", "At the bottom", "Frankie's pub", "China"],
+//     corAnswer: 0,
+//     usersAnswer: null
+// }];
+questions.forEach(question => {
+    question.usersAnswer = null;
+});
+
