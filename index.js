4c7fe9a802c65a979ddbf5d93add5976027d13d4
Shinkaruk Anna <sklifa1087@gmail.com>
2026-06-10 11:20:55 +0300

    commit task-2

--- index.js
+++ index.js
@@ -76,3 +76,12 @@ function arrayPlusArray(arr1, arr2) {
 function solution(str){
     return str.split("").reverse().join("")
 }
+
+// Give you a function animal, accept 1 parameter:obj like this:
+// {name:"dog",legs:4,color:"white"}
+// and return a string like this:
+// "This white dog has 4 legs."
+function animal(obj){
+    return "This " + obj.color + " " + obj.name + " has " + obj.legs + " legs.";
+
+}
