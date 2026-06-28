a97b3c7f6fdb961329394cefa2e61b5bae9b0555
Shinkaruk Anna <sklifa1087@gmail.com>
2026-06-28 21:55:19 +0300

    commit task-1

--- index.js
+++ index.js
@@ -126,4 +126,60 @@ function addProperty(obj, prop, value) {
     enumerable: true,
     configurable: true
     });
+
 }
+
+    // Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+    // If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+    // Input = ["Ryan", "Kieran", "Jason", "Yous"]
+    // Output = ["Ryan", "Yous"]
+
+    // Input = ["Peter", "Stephen", "Joe"]
+    // Output = []
+
+    function friend(friends){
+        return friends.filter(name => name.length === 4);
+}
+
+// Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+// For example, if this array were passed as an argument:
+
+// ["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+function sortByLength (array) {
+  // Return an array containing the same strings,
+  // ordered from shortest to longest
+    return array.sort((a, b) => a.length - b.length);
+}
+
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+function descendingOrder(n){
+    return Number(n.toString().split('').sort((a, b) => b - a).join(''));
+}
+
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+function filter_list(l) {
+    return l.filter(item => typeof item !== 'string');
+}
+
+
+
+
+
+
+
